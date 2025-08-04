d1d152f24ba198ef0b98aa02917b24a40b4a14f3
Andrey <andreikabantsev@yandex.ru>
2025-08-04 23:18:55 +0300

    Добавлены вске файлы

---
+++ src/main/java/webmvc/controller/UserController.java
@@ -0,0 +1,43 @@
+package webmvc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import webmvc.model.User;
+import webmvc.service.UserService;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public String index(Model model) {
+        System.out.printf("index");
+        model.addAttribute("users", userService.findAll());
+        model.addAttribute("newUser", new User());
+        model.addAttribute("updateUser", new User());
+        return "users";
+    }
+
+    @PostMapping
+    public String save(@ModelAttribute User user) {
+        userService.save(user);
+        return "redirect:/users";
+    }
+
+    @PutMapping
+    public String update(@ModelAttribute User user) {
+        userService.update(user);
+        return "redirect:/users";
+    }
+
+    @DeleteMapping
+    public String delete(@RequestParam int id) {
+        userService.delete(id);
+        return "redirect:/users";
+    }
+}
---
+++ src/main/java/webmvc/dao/UserDaoImpl.java
@@ -0,0 +1,41 @@
+package webmvc.dao;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import webmvc.model.User;
+
+import java.util.List;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+   @Transactional
+    @Override
+    public List<User> findAll() {
+       return sessionFactory.getCurrentSession().createQuery("from User", User.class).getResultList();
+    }
+
+    @Transactional
+    @Override
+    public void save(User user) {
+       sessionFactory.getCurrentSession().merge(user);
+    }
+
+    @Transactional
+    @Override
+    public void delete(int id) {
+       User user = sessionFactory.getCurrentSession().get(User.class, id);
+       sessionFactory.getCurrentSession().remove(user);
+    }
+
+    @Transactional
+    @Override
+    public User findById(int id) {
+        return sessionFactory.getCurrentSession().get(User.class, id);
+    }
+}
